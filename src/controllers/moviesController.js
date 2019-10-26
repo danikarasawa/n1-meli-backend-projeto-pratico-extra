3a2304d1bd3ca48cb2af1f125611087b5c5c7ec0
danikarasawa <danikarasawa@gmail.com>
2019-10-25 22:07:28 -0200

    homework

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -9,7 +9,20 @@ exports.getById = (req, res) => {
     const id = req.params.id
 
     if (id > 4 || id <= 0) {
-        res.redirect(301, "https://cdn.searchenginejournal.com/wp-content/uploads/2017/08/1.-Pixar-Animation-Studios-Google-Chrome.png") 
+        res.redirect(301, "https://cdn.searchenginejournal.com/wp-content/uploads/2017/08/1.-Pixar-Animation-Studios-Google-Chrome.png")
     }
     res.status(200).send(movies.find(item => item.id == id))
+}
+
+exports.getByType = (req, res) => {
+    const id = req.params.id
+    const eita = movies.find(item => item.id == id)
+    if (!titulo) {
+        res.redirect(301, "https://cdn.searchenginejournal.com/wp-content/uploads/2017/08/1.-Pixar-Animation-Studios-Google-Chrome.png")
+    } else {
+        const movieGen = titulo.genero
+        const movieGenOk = movieGen.filter(item => item.aplica == "true")
+        const tipoGen = movieGenOk.map(item => item.tipo)
+        res.status(200).send(tipoGen)
+    }
 }
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -4,5 +4,5 @@ const moviesController = require('../controllers/moviesController')
 
 router.get('/', moviesController.get) 
 router.get('/:id', moviesController.getById)
-
+router.get('/genero/:tipo', moviesController.getByType)
 module.exports = router
